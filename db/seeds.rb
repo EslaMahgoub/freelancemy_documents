58b5489ec76b0cb96055bdf312348b7f4830abf5
exit <exit>
2021-09-16 00:25:48 +0200

    Generate data
    Allow CORS

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# create_table "freelancemy_documents", force: :cascade do |t|
+#   t.string "title"
+#   t.string "description"
+#   t.text "file_url"
+#   t.text "image_url"
+#   t.datetime "created_at", precision: 6, null: false
+#   t.datetime "updated_at", precision: 6, null: false
+# end
+
+10.times do
+  FreelancemyDocument.create!([{
+    title: Faker::JapaneseMedia::Conan.character,
+    description: Faker::TvShows::GameOfThrones.quote,
+    file_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUDo5SztXNhU-lS_-nynr-GRKp5E9oQpq_lQ&usqp=CAU",
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUDo5SztXNhU-lS_-nynr-GRKp5E9oQpq_lQ&usqp=CAU",
+  }])
+end
